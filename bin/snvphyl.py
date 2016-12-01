3088f272da63007ce4c01811073d3ee28dfab3dd
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2016-12-01 13:40:40 -0600

    version to 1.0.0

--- bin/snvphyl.py
+++ bin/snvphyl.py
@@ -11,7 +11,7 @@ from bioblend.galaxy import dataset_collections
 
 polling_time=10 # seconds
 use_newer_galaxy_api=False
-snvphyl_cli_version='1.0.1-prerelease'
+snvphyl_cli_version='1.0.0'
 
 galaxy_api_key_name='--galaxy-api-key'
 
